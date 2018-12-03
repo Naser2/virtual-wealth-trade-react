6772db78393425aba2d31325caf53c76cc33a905
Nas <nassersanou23@gmail.com>
2018-12-03 10:50:11 -0500

    Token Refactor

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -5,7 +5,20 @@ export default class SearchBar extends Component {
   render() {
     return (
       <div>
-          <input name="searchInput" value={this.props.searchData} onChange={this.props.getSearchedCurrencies} /> <br />
+          <input name="searchInput" value={this.props.searchData} onChange={this.props.getSearchedCurrencies} 
+          style={{
+            margin: '0 auto',
+            maxWidth: '800',
+            color: 'black',
+            width: '100',
+            position: 'relative ' ,
+            height: '40px',
+            border:' 1px solid #00B4CC',
+            background: '#white',
+            borderRadius: '3px',
+            cursor: 'pointer',
+            fontSize: '30px'
+      }}/> <br />
       </div>
     )
   }
