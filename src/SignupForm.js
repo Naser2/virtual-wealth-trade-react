45eb0f4b915d2fee015be1801dea6af53128f99b
Nas <nassersanou23@gmail.com>
2018-12-05 22:16:43 -0500

    added temporary style to the SignUp page

--- src/SignupForm.js
+++ src/SignupForm.js
@@ -15,15 +15,36 @@ export default class SignupForm extends Component {
      })
   }
 
+  // <form onSubmit={(e)=>this.props.handleSubmit(e,this.state)}>
+  // <h1>Please Sign</h1>
+  // <input type="text" name="name" placeholder="name" value={this.state.name}  onChange={this.handleChange}/>
+  // <input type="text" name="username" placeholder="username" value={this.state.username} onChange={this.handleChange}/>
+  // <input type="text" name="password" placeholder="password" value={this.state.password} onChange={this.handleChange} />
+  // <input type="submit" />
+  // </form>
   render() {
     return (
-      <form onSubmit={(e)=>this.props.handleSubmit(e,this.state)}>
-        <h1>Please Sign</h1>
-        <input type="text" name="name" placeholder="name" value={this.state.name}  onChange={this.handleChange}/>
-        <input type="text" name="username" placeholder="username" value={this.state.username} onChange={this.handleChange}/>
-        <input type="text" name="password" placeholder="password" value={this.state.password} onChange={this.handleChange} />
-        <input type="submit" />
-        </form>
+      <div class="ui grid container middle aligned" >
+      <div class="column">
+      <div class="ui segment top attached"> Sign Up</div>
+        <form class="ui form segment top attached green" onSubmit={(e)=>this.props.handleSubmit(e,this.state)}>
+          <div class="field ui left icon input fluid">
+          <input type="text" name="name" placeholder="name" value={this.state.name}  onChange={this.handleChange}/>
+          <br />
+            <input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.handleChange}/>
+            <i class="users icon"></i>
+          </div>
+          <div class="field ui left icon input fluid">
+            <input type="text" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} />  
+            <i class="key icon"></i>
+          </div>
+          <button class="ui button fluid positive" type="submit" value="Login"style={{background: '#0084cc'}} >Submit</button>
+        </form>  
+        <div class="ui segment bottom attached"> <a >Welcome to Virtual Weatlth</a>.</div>
+      </div>
+    </div>
+  
+  
 
     )
   }
