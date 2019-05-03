c1c5ca0b6dd59d71f34c3931abe7a8f450b4659b
Nas <nassersanou23@gmail.com>
2019-05-03 15:47:31 -0400

    updates

--- src/components/presentational/CurrencyCollection.js
+++ src/components/presentational/CurrencyCollection.js
@@ -10,6 +10,7 @@ export default class CurrencyCollection extends Component {
     selectedCoins: []
   };
   onConfirm = () => {};
+
   handleCoinsChange = event => {
     const itemId = event.target.id;
     const coin = this.props.cryptos.find(c => c.id == itemId);
@@ -31,31 +32,40 @@ export default class CurrencyCollection extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    const { username } = this.props.username;
-    console.log('ccccc',username );
-    console.log('CURENCYIES : ', this.props);
+    if(this.props.active){
+
+    }
+  
     // e.preventDefault();
 
-    if (this.props.active) {
-      return (
-        <SweetAlert title="Here's a message!" onConfirm={this.onConfirm} />
-      );
-    } else {
-      const { username } = this.props.username;
+    // if (this.props.active) {
+    //   return (
+    //     <SweetAlert title="Here's a message!" onConfirm={this.onConfirm} />
+    //   );
+    // } 
+    
+    
+    if(this.props.active) {
+      // const { username } = this.props.username;
+      // console.log('ccccc',username );
+      console.log('CURENCYIES : ', this.props);
+
+
+      const username = this.props.username;
       const assets = {
-        [username]: [...this.state.selectedCoins]
+        [this.props.username]: [...this.state.selectedCoins]
       };
       let oldAssets = localStorage.getItem('assets');
       if (oldAssets) {
         oldAssets = JSON.parse(oldAssets);
         //if user already saved assets
-        if (oldAssets[username]) {
+        if (oldAssets[this.props.username]) {
           //if user has assets in localStorage
 
           this.state.selectedCoins.forEach(c => {
-            console.log('ccccc', c, 'oldddddd', oldAssets[username]);
+            console.log('ccccc', c, 'oldddddd', oldAssets[this.props.username]);
             if (
-              oldAssets[username].find(a => {
+              oldAssets[this.props.username].find(a => {
                 console.log('ew Coinnn', c);
                 console.log('Old coinnn', a);
                 return a.id === c.id;
@@ -64,9 +74,9 @@ export default class CurrencyCollection extends Component {
               console.log(`${c.name} this coin was already added`);
             } else {
               console.log(`${c.name} This coin was not  added`);
-              oldAssets[username] = [
+              oldAssets[this.props.username] = [
                 //lets add the existing assets + the
-                ...oldAssets[username],
+                ...oldAssets[this.props.username],
                 c
               ];
             }
@@ -78,12 +88,15 @@ export default class CurrencyCollection extends Component {
       } else {
         localStorage.setItem('assets', JSON.stringify(assets));
       }
+      
     }
   };
 
   btnMessage = () => {
+
     alert('You need to LOGIN to Save');
   };
+
   verifySaveBtnOption = () => {
     if (this.props.profile) {
       return null;
@@ -109,7 +122,9 @@ export default class CurrencyCollection extends Component {
   render() {
     let { cryptos } = this.props;
 
-    // console.log("Crypto IDDDDD:", cryptos)
+console.log("CRYPTO PROPS  :", crypto, "PROPS: ",  this.props)
+    console.log("PROPS DOT PROFILE :", this.props.profile && this.props.active)
+    console.log("PROPS DOT ACTIVE :",  this.props.active)
     let cryptoArr = cryptos.map(cryptoObj => {
       return (
         <Currency
