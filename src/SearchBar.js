5f62789ce2e8f8ecff9194e578fb5d333f4d3ba0
Nas <nassersanou23@gmail.com>
2019-05-04 02:19:48 -0400

    search css

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,29 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
-  
   render() {
     return (
       <div>
-          <input name="searchInput" value={this.props.searchData} onChange={this.props.getSearchedCurrencies}
+        <input
+          name="searchInput"
+          value={this.props.searchData}
+          onChange={this.props.getSearchedCurrencies}
           placeholder="Search..."
           style={{
             margin: '0 auto',
             maxWidth: '800',
             color: 'black',
             width: '100',
-            position: 'relative ' ,
+            position: 'relative ',
             height: '40px',
-            border:' 2.5px solid #00B4CC',
+            border: ' 2.5px solid #00B4CC',
+            border: '4px solid #4286f4',
             background: '#white',
             borderRadius: '3px',
             cursor: 'pointer',
             fontSize: '30px',
             padding: '26px',
             borderRadius: '30px',
-      }}/> <br />
+            marginBottom: '15px'
+          }}
+        />{' '}
+        <br />
       </div>
-    )
+    );
   }
 }
-
